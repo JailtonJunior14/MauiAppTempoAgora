e8fcb2202cc47444ac34153b8cfd85a4d5d0369a
JailtonJunior14 <jailtonjrbr@gmail.com>
2025-04-07 11:30:47 -0300

    Adicionar arquivos de projeto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿
+using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+using System;
+using System.Diagnostics;
+
+namespace MauiAppTempoAgora
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked_Localizacao(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void Button_Clicked_Previsao(object sender, EventArgs e)
+        {
+
+
+        }
+    }
+
+}
